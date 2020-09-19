db62566f0fa27ed29bb7fca0ffa548b362721e5c
Adarsh <herecomesthebeastlesnar@gmail.com>
2020-09-19 14:48:01 +0530

    Define Folder Structure

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("erxpress");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const usersRoutes = require("./routes/user-routes");
+const linksRoutes = require("./routes/link-routes");
