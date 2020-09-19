bf34081c6119a2e03f39c19b92823c156b7d2510
Adarsh <herecomesthebeastlesnar@gmail.com>
2020-09-19 15:07:15 +0530

    Basic Setup Done

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const express = require("erxpress");
+const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const usersRoutes = require("./routes/user-routes");
 const linksRoutes = require("./routes/link-routes");
+const HttpError = require("./models/http-error");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.use("/api/users", usersRoutes);
+app.use("/api/links", linksRoutes);
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find the specified Route.", 404);
+});
+
+app.listen(3000);
