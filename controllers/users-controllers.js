9f38a3f565e3c8b58e39eca97634d5a14bbb84af
Adarsh <herecomesthebeastlesnar@gmail.com>
2020-09-19 16:04:22 +0530

    Done Signup

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,3 +1,87 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
+const HttpError = require("../models/http-error");
+const User = require("../models/user");
+
+const signup = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new HttpError(
+      "Invalid inputs passed, please try again later.",
+      422
+    );
+    return next(error);
+  }
+
+  const { name, email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ emaoil: email });
+  } catch (err) {
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      422
+    );
+    return next(error);
+  }
+
+  if (existingUser) {
+    const error = new HttpError(
+      "User exists already, please login instead.",
+      422
+    );
+    return next(error);
+  }
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not create user, please try again.",
+      500
+    );
+    return next(error);
+  }
+
+  const createdUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+    links: [],
+  });
+
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      process.env.JWT_KEY,
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token: token });
+};
+
+exports.signup = signup;
